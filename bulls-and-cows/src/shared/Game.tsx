d42a9ef1f5b417ffcaaa7d01228494b8dd4b1d6d
dkashif <dkashif@arizona.edu>
2025-10-18 12:50:23 -0700

    changes game.tsx

--- bulls-and-cows/src/shared/Game.tsx
+++ bulls-and-cows/src/shared/Game.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 
 export const Game = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       {[...Array(4).keys()].map((key) => (
-        <div>
-          <label htmlFor={key.toString()}></label>
-          <input id={key.toString()} type="text"  />
+        <div key={key} className="flex flex-col">
+          <label htmlFor={key.toString()} className="mb-1 text-gray-700">
+            Input {key + 1}
+          </label>
+          <input
+            id={key.toString()}
+            type="text"
+            className="bg-gray-100 border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-red-400 transition-colors"
+          />
         </div>
       ))}
     </div>
-  )
-
-}
+  );
+};
