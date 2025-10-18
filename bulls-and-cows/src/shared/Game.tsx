98a208d4444d3320f3e83d3b7f7b974e10c8143c
Elton-Ho <eltonho77@gmail.com>
2025-10-18 12:41:22 -0700

    hide guess

--- bulls-and-cows/src/shared/Game.tsx
+++ bulls-and-cows/src/shared/Game.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-function getRandomInt(max: number): number {
-  return Math.floor(Math.random() * max);
-}
-
 
 export const Game = () => {
   return (
     <div>
-      {[...Array(10).keys()].map((key) => (
+      {[...Array(4).keys()].map((key) => (
         <div>
-          <label htmlFor={key.toString()}>{getRandomInt(10)}</label>
+          <label htmlFor={key.toString()}></label>
           <input id={key.toString()} type="text"  />
         </div>
       ))}
