f37ae15d2f4a4cc92e2adff3e852fd39fa9db792
Young <113062069+ArmsTina@users.noreply.github.com>
2025-10-18 12:08:48 -0700

    React Router Added

--- bulls-and-cows/src/components/Header.tsx
+++ bulls-and-cows/src/components/Header.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className="bg-black border-b-gray-500 border-4 sticky top-0 z-50">
+    <header className="bg-black border-b-gray-500 border-b-4 sticky top-0 z-50">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0">
-            <a
-              href="/"
+            <Link
+              to="/"
               className="text-2xl font-extrabold text-green-700 hover:text-green-500 transition duration-150"
             >
               Bulls and Cows
-            </a>
+            </Link>
           </div>
         </div>
       </div>
